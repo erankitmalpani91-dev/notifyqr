3fb513856f0867f5b6c81d9fb0449cb265db8723
Ankit Malpani <erankitmalpani91@gmail.com>
2026-02-14 16:14:47 +0530

    Fix path import and static directory paths

--- app/server.js
+++ app/server.js
@@ -3,7 +3,10 @@ const sqlite3 = require("sqlite3").verbose();
 const twilio = require("twilio");
 const QRCode = require("qrcode");
 const session = require("express-session");
+const path = require("path");
 require("dotenv").config();
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,8 +14,8 @@ const PORT = process.env.PORT || 3000;
 /* -------------------- MIDDLEWARE -------------------- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // REQUIRED for Twilio
-app.use("/", express.static(path.join(__dirname, "../../Website")));
-app.use("/app", express.static(path.join(__dirname)));
+app.use("/", express.static(path.join(__dirname, "../Website")));
+app.use("/app", express.static(path.join(__dirname,"public")));
 
 
 app.use(
