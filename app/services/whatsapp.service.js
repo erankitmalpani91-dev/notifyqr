541b08f3628cf0b195a8789c6c28738229f7f5f2
Ankit Malpani <erankitmalpani91@gmail.com>
2026-04-19 01:02:55 +0530

    en and en_US error fixed in Whatsapp Services

--- app/services/whatsapp.service.js
+++ app/services/whatsapp.service.js
@@ -30,7 +30,7 @@ async function sendWhatsApp(to, data) {
                 type: "template",
                 template: {
                     name: templateName,
-                    language: { code: "en" },
+                    language: { code: "en_US" },
                     components: [
                         {
                             type: "body",
